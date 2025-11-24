4f23fb66719e5e228ebe5640d2439b5ef1dd9175
lasertsole <3132225629@qq.com>
2025-11-24 22:00:12 +0800

    editor_create

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-    123
+    <div class="root">
+        <editor></editor>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.root {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
