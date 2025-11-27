815cfbe8d57eb254101a76c4a2dbd08e63138bed
MOYE <56973455+lasertsole@users.noreply.github.com>
2025-11-27 13:54:29 +0800

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,24 +1,43 @@
 <template>
-  <div id="root">
-    <NuxtLayout>
-      <NuxtPage keepalive />
-    </NuxtLayout>
+  <div class="app">
+    <div id="topLayer"></div>
+    <div id="contentLayer">
+      <NuxtLayout>
+        <NuxtPage keepalive />
+      </NuxtLayout>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-#root {
+.app {
+  position: relative;
+  
   @include fullViewWindow;
-
-  .page-enter-active,
-  .page-leave-active {
-    transition: all 0.4s;
+  #topLayer{
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    @include fullInParent;
+    z-index: 2;
+    background-color: transparent !important;
   }
 
-  .page-enter-from,
-  .page-leave-to {
-    opacity: 0;
-    filter: blur(1rem);
+  #contentLayer{
+    position: relative;
+    @include fullInParent;
+    z-index: 1;
+
+    .page-enter-active,
+    .page-leave-active {
+      transition: all 0.4s;
+    }
+  
+    .page-enter-from,
+    .page-leave-to {
+      opacity: 0;
+      filter: blur(1rem);
+    }
   }
 }
 </style>
