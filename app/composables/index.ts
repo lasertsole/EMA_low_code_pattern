8778fe07b18adcce421bef460d8feccad155da24
MOYE <56973455+lasertsole@users.noreply.github.com>
2025-11-27 09:26:56 +0800

    Update index.ts

--- app/composables/index.ts
+++ app/composables/index.ts
@@ -3,33 +3,32 @@ import { mapValues, isNil } from "lodash-es"
 
 export const commonDefaultProps:ComponentProps = {
     // size
-    height: '',
-    width: '',
-   maxHeight: '',
-   maxWidth: '',
-   minHeight: '',
-   minWidth: '',
+   height: 'auto',
+   width: 'auto',
+   maxHeight: 'auto',
+   maxWidth: 'auto',
+   minHeight: 'auto',
+   minWidth: 'auto',
    // position
-   position: '',
+   position: 'static',
    top: '0px',
    right: '0px',
    bottom: '0px',
    left: '0px',
    // border
-   border: '',
-   borderRadius: '',
-   borderTop: '',
-   borderRight: '',
-   borderBottom: '',
-   borderLeft: '',
-    borderColor: '',
-    borderWidth: '',
-    // shadow and opacity
-    boxShadow: '',
-    opacity: '',
-    // geometric
-    backgroundColor: '',
-
+   border: '0',
+   borderRadius: '0',
+   borderTop: 'initial',
+   borderRight: 'initial',
+   borderBottom: 'initial',
+   borderLeft: 'initial',
+   borderColor: 'initial',
+   borderWidth: 'initial',
+   // shadow and opacity
+   boxShadow: 'initial',
+   opacity: 'initial',
+   // geometric
+   backgroundColor: 'initial',
 }
 
 // 将默认值props 转换成组件的 props
@@ -40,13 +39,13 @@ export const textDefaultProps:ComponentProps = {
     textAlign: 'left',
     color: '#000',
     textDecoration: 'none',
-    fontFamily: '',
-    fontSize: '',
-    fontWeight: '',
-    lineHeight: '',
-    letterSpacing: '',
-    textIndent: '',
-    textOverflow: '',
+    fontFamily: 'initial',
+    fontSize: 'initial',
+    fontWeight: 'initial',
+    lineHeight: 'initial',
+    letterSpacing: 'initial',
+    textIndent: 'initial',
+    textOverflow: 'initial',
 }
 
 // 将默认值props 转换成组件的 props
